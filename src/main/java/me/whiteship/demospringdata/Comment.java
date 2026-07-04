52693d40f9b9962f80a792d2e1fb83c154b8a9ce
Bruce Han <intragerhan@gmail.com>
2026-07-04 21:00:47 +0900

    엔티티 상태와 Cascade

---
+++ src/main/java/me/whiteship/demospringdata/Comment.java
@@ -0,0 +1,22 @@
+package me.whiteship.demospringdata;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Comment {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String comment;
+
+    @ManyToOne
+    private Post post;
+}
--- src/main/java/me/whiteship/demospringdata/JpaRunner.java
+++ src/main/java/me/whiteship/demospringdata/JpaRunner.java
@@ -2,12 +2,14 @@ package me.whiteship.demospringdata;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Component
 @Transactional
 public class JpaRunner implements ApplicationRunner {
@@ -16,18 +18,35 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Account account = new Account();
-        account.setUsername("brucehan");
-        account.setPassword("hibernate");
+//        Account account = new Account();
+//        account.setUsername("brucehan");
+//        account.setPassword("hibernate");
+//
+//        Study study = new Study();
+//        study.setName("Spring Data JPA");
+//
+//        account.addStudy(study);
+//
+//        Session session = em.unwrap(Session.class);
+//        session.persist(account);
+//        session.persist(study);
+////        em.persist(account);
+//        Account brucehan = session.find(Account.class, account.getId());
+//        brucehan.setUsername("brrrrrsan");
+//        log.info("brucehan: {}", brucehan.getUsername());
 
-        Study study = new Study();
-        study.setName("Spring Data JPA");
+        Post post = new Post();
+        post.setTitle("Spring Data JPA");
 
-        account.addStudy(study);
+        Comment comment = new Comment();
+        comment.setComment("JPA");
+        post.addComment(comment);
+
+        Comment comment1 = new Comment();
+        comment1.setComment("스프링 데이터 제이피에이");
+        post.addComment(comment1);
 
         Session session = em.unwrap(Session.class);
-        session.persist(account);
-        session.persist(study);
-//        em.persist(account);
+        session.persist(post);
     }
 }
